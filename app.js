7569cc78f86935a837321b9eefdb728c6147dc7f
Roy <165288975+Auresol69@users.noreply.github.com>
2024-11-03 02:01:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,9 @@ const paymentData = [
             <label for="location">Location delivery</label>
             <br>
                 <input type="text" id="location" title="Please enter your location" placeholder="123/A Hau Giang Street, P11, Q6, TP.HCM">
-        </div>`,
+        </div>
+        <div class="error-message" id="error-locat-delivery"></div>
+`,
     `<div class="receipt-container">
     <h2>Your Receipt</h2>
     <p>Thank you for your purchase!</p>
@@ -68,17 +70,20 @@ function handleRadioChange(event) {
                             <label for="momo_phone">Phone number</label>
                             <input type="text" name="momo_phone" id="momo_phone" inputmode="numeric" pattern="[0-9]*"
                                 title="Please enter Phone number" placeholder="Number" required
-                                oninput="this.value = this.value.replace(/[^0-9]/g,'')">
+                                oninput="this.value = this.value.replace(/[^0-9]/g,'')" maxlength="11">
+                            <div class="error-message" id="error-momo_phone"></div>
                         </div>
                         <div class="payment__option__item">
                             <label for="momo_name">Name</label>
                             <input type="text" name="momo_name" id="momo_name" title="Please enter Name" placeholder="Name" required>
-                        </div>
+                        <div class="error-message" id="error-momo_name"></div>
+                            </div>
                         <div class="payment__option__item">
                             <label for="momo_pin">Pin code</label>
                             <input type="text" name="momo_pin" id="momo_pin" inputmode="numeric" pattern="[0-9]*"
                                 title="Please enter Pin code" placeholder="******" required
                                 oninput="this.value = this.value.replace(/[^0-9]/g,'')" maxlength="6">
+                            <div class="error-message" id="error-momo_pin"></div>
                         </div>
                     `;
             break;
@@ -90,35 +95,41 @@ function handleRadioChange(event) {
                             <input type="text" inputmode="numeric" name="atm_card_number" id="atm_card_number" pattern="[0-9]*"
                                 title="Please enter Card number" placeholder="Card number" required
                                 oninput="this.value = this.value.replace(/[^0-9]/g, '')">
+                                                        <div class="error-message" id="error-atm_card_number"></div>
                         </div>
                         <div class="payment__option__item">
                             <label for="atm_expired_date">Expired date</label>
-                            <input type="text" inputmode="numeric" name="atm_expired_date" id="atm_expired_date" pattern="[0-9]*"
-                                title="Please enter Expired date" placeholder="MM/YY" required
-                                oninput="this.value = this.value.replace(/[^0-9/]/g, '')" maxlength="5">
-                        </div>
+                            <input type="text" name="atm_expired_date" id="atm_expired_date" pattern="(?:0[0-9]|1[0-2])/[0-9]{2}"
+                            title="Please enter Expired date (MM/YY)" placeholder="MM/YY" required
+                            oninput="this.value = this.value.replace(/[^0-9/]/g, '')" maxlength="5">
+                        <div class="error-message" id="error-atm_expired_date"></div>
+                                </div>
                         <div class="payment__option__item">
                             <label for="atm_cvc">CVC</label>
                             <input type="text" name="atm_cvc" id="atm_cvc" inputmode="numeric" pattern="[0-9]*"
                                 title="Please enter CVC" placeholder="CVC" required
                                 oninput="this.value = this.value.replace(/[^0-9]/g, '')" maxlength="4">
-                        </div>
+                        <div class="error-message" id="error-atm_cvc"></div>
+                                </div>
                         <div class="payment__option__item">
                             <label for="atm_phone">Phone number</label>
                             <input type="text" name="atm_phone" id="atm_phone" inputmode="numeric" pattern="[0-9]*"
                                 title="Please enter Phone number" placeholder="Number" required
-                                oninput="this.value = this.value.replace(/[^0-9]/g,'')">
-                        </div>
+                                oninput="this.value = this.value.replace(/[^0-9]/g,'')" maxlength="11">
+                        <div class="error-message" id="error-atm_phone"></div>
+                                </div>
                         <div class="payment__option__item">
                             <label for="atm_pin">Pin code</label>
                             <input type="text" name="atm_pin" id="atm_pin" inputmode="numeric" pattern="[0-9]*"
                                 title="Please enter Pin code" placeholder="******" required
                                 oninput="this.value = this.value.replace(/[^0-9]/g,'')" maxlength="6">
+                        <div class="error-message" id="error-atm_pin"></div>
                         </div>
                         <div class="payment__option__item">
                             <label for="atm_name">Name</label>
                             <input type="text" name="atm_name" id="atm_name" title="Please enter Name" placeholder="Name" required>
-                        </div>
+                        <div class="error-message" id="error-atm_name"></div>
+                            </div>
                     `;
             break;
 
@@ -128,17 +139,13 @@ function handleRadioChange(event) {
                             <label for="cash_phone">Phone number</label>
                             <input type="text" name="cash_phone" id="cash_phone" inputmode="numeric" pattern="[0-9]*"
                                 title="Please enter Phone number" placeholder="Number" required
-                                oninput="this.value = this.value.replace(/[^0-9]/g,'')">
+                                oninput="this.value = this.value.replace(/[^0-9]/g,'')" maxlength="11">
+                        <div class="error-message" id="error-cash_phone"></div>                        
                         </div>
                         <div class="payment__option__item">
                             <label for="cash_name">Name</label>
                             <input type="text" name="cash_name" id="cash_name" title="Please enter Name" placeholder="Name" required>
-                        </div>
-                        <div class="payment__option__item">
-                            <label for="cash_amount">Amount</label>
-                            <input type="text" inputmode="numeric" name="cash_amount" id="cash_amount" pattern="[0-9]*"
-                                title="Please enter Amount" placeholder="Amount" required
-                                oninput="this.value = this.value.replace(/[^0-9]/g, '')">
+                        <div class="error-message" id="error-cash_name"></div>                        
                         </div>
                         <div class="payment__option__item">
                             <label for="cash_note">Note (optional)</label>
@@ -182,7 +189,6 @@ function saveDataToLocalStorage() {
             case 'Cash':
                 paymentInfo.phone = document.getElementById('cash_phone').value;
                 paymentInfo.name = document.getElementById('cash_name').value;
-                paymentInfo.amount = document.getElementById('cash_amount').value;
                 paymentInfo.note = document.getElementById('cash_note').value;
                 break;
         }
@@ -199,7 +205,7 @@ function saveDataToLocalStorage() {
 function getPaymentInfoFromLocalStorage() {
     const storedPaymentInfo = localStorage.getItem('paymentInfo');
     return storedPaymentInfo ? JSON.parse(storedPaymentInfo) : null;
-  }
+}
 
 function clearData() {
     getPaymentInfoFromLocalStorage();
@@ -214,10 +220,9 @@ function clearData() {
     const atmName = document.getElementById('atm_name');
     const cashPhone = document.getElementById('cash_phone');
     const cashName = document.getElementById('cash_name');
-    const cashAmount = document.getElementById('cash_amount');
     const cashNote = document.getElementById('cash_note');
-    
-    if (currentIndex === 1){
+
+    if (currentIndex === 1) {
         for (let key in paymentInfo) {
             delete paymentInfo[key];
         }
@@ -233,11 +238,10 @@ function clearData() {
         if (atmName) atmName.value = '';
         if (cashPhone) cashPhone.value = '';
         if (cashName) cashName.value = '';
-        if (cashAmount) cashAmount.value = '';
         if (cashNote) cashNote.value = '';
     }
-    
-    if (currentIndex===2) {
+
+    if (currentIndex === 2) {
         delete paymentInfo.location;
         const locationInput = document.getElementById('location');
         if (locationInput) locationInput.value = '';
@@ -259,14 +263,14 @@ prevButton.addEventListener('click', () => {
 });
 
 nextButton.addEventListener('click', () => {
-        if (currentIndex < paymentData.length - 1) {
-            saveDataToLocalStorage();
-            currentIndex++;
-            updateContent();
-            if (currentIndex === 2)
+    if (validateInputs()){ if (currentIndex < paymentData.length - 1) {
+        saveDataToLocalStorage();
+        currentIndex++;
+        updateContent();
+        if (currentIndex === 2)
             showInfo();
     }
-    console.log(currentIndex);
+    console.log(currentIndex);}
 });
 
 function showInfo() {
@@ -296,3 +300,126 @@ function notShowInfo() {
 }
 
 updateContent();
+
+function validateInputs() {
+    let isValid = true;
+    const locatDelivery = document.getElementById('location');
+    if (locatDelivery && locatDelivery.value === '') {
+        document.getElementById('error-locat-delivery').innerText = 'Location delivery is required';
+        document.getElementById('error-locat-delivery').style.display = 'block';
+        return false;
+    }
+    
+    else if (locatDelivery)
+    {
+        document.getElementById('error-locat-delivery').style.display = 'none';
+        return true;
+    }
+    switch (currentMethod) {
+        case 'Momo':
+            const momoPhone = document.getElementById('momo_phone');
+            const momoName = document.getElementById('momo_name');
+            const momoPin = document.getElementById('momo_pin');
+            
+           if (momoPhone && momoPhone.value === '') {
+    document.getElementById('error-momo_phone').innerText = 'Phone number is required';
+    document.getElementById('error-momo_phone').style.display = 'block';
+    isValid = false;
+} else if (momoPhone) {
+    document.getElementById('error-momo_phone').style.display = 'none';
+           }
+
+if (momoName && momoName.value === '') {
+    document.getElementById('error-momo_name').innerText = 'Name is required';
+    document.getElementById('error-momo_name').style.display = 'block';
+    isValid = false;
+} else if (momoName) {
+    document.getElementById('error-momo_name').style.display = 'none';
+   
+}
+
+if (momoPin && (momoPin.value === '' || momoPin.value.length < 6)) {
+    document.getElementById('error-momo_pin').innerText = 'Pin code must be 6 digits';
+    document.getElementById('error-momo_pin').style.display = 'block';
+    isValid = false;
+} else if (momoPin) {
+    document.getElementById('error-momo_pin').style.display = 'none';
+}
+            return isValid;
+        case 'ATM':
+            const atmCardNumber = document.getElementById('atm_card_number');
+            const atmExpiredDate = document.getElementById('atm_expired_date');
+            const atmCvc = document.getElementById('atm_cvc');
+            const atmPhone = document.getElementById('atm_phone');
+            const atmPin = document.getElementById('atm_pin');
+            const atmName = document.getElementById('atm_name');
+    if (atmCardNumber && atmCardNumber.value === '') {
+    document.getElementById('error-atm_card_number').innerText = 'Card number is required';
+    document.getElementById('error-atm_card_number').style.display = 'block';
+    isValid = false;
+} else if (atmCardNumber) {
+    document.getElementById('error-atm_card_number').style.display = 'none';
+}
+
+if (atmExpiredDate && atmExpiredDate.value === '') {
+    document.getElementById('error-atm_expired_date').innerText = 'Expired date is required';
+    document.getElementById('error-atm_expired_date').style.display = 'block';
+    isValid = false;
+} else if (atmExpiredDate) { 
+    document.getElementById('error-atm_expired_date').style.display = 'none';
+}
+
+if (atmCvc && (atmCvc.value === '' || atmCvc.value.length < 3)) {
+    document.getElementById('error-atm_cvc').innerText = 'CVC must be at least 3 digits';
+    document.getElementById('error-atm_cvc').style.display = 'block';
+    isValid = false;
+} else if (atmCvc) {
+    document.getElementById('error-atm_cvc').style.display = 'none';
+}
+
+if (atmPhone && atmPhone.value === '') {
+    document.getElementById('error-atm_phone').innerText = 'Phone number is required';
+    document.getElementById('error-atm_phone').style.display = 'block';
+    isValid = false;
+} else if (atmPhone) {
+    document.getElementById('error-atm_phone').style.display = 'none';
+}
+
+if (atmPin && (atmPin.value === '' || atmPin.value.length < 6)) {
+    document.getElementById('error-atm_pin').innerText = 'Pin code must be 6 digits';
+    document.getElementById('error-atm_pin').style.display = 'block';
+    isValid = false;
+} else if (atmPin) {
+    document.getElementById('error-atm_pin').style.display = 'none';
+}
+
+if (atmName && atmName.value === '') {
+    document.getElementById('error-atm_name').innerText = 'Name is required';
+    document.getElementById('error-atm_name').style.display = 'block';
+    isValid = false;
+} else if (atmName) {
+    document.getElementById('error-atm_name').style.display = 'none';
+}
+            return isValid;
+        case 'Cash':
+            const cashPhone = document.getElementById('cash_phone');
+            const cashName = document.getElementById('cash_name');
+    
+if (cashPhone && cashPhone.value === '') {
+    document.getElementById('error-cash_phone').innerText = 'Phone number is required';
+    document.getElementById('error-cash_phone').style.display = 'block';
+    isValid = false;
+} else if (cashPhone) {
+    document.getElementById('error-cash_phone').style.display = 'none';
+}
+
+if (cashName && cashName.value === '') {
+    document.getElementById('error-cash_name').innerText = 'Name is required';
+    document.getElementById('error-cash_name').style.display = 'block';
+    isValid = false;
+} else if (cashName) {
+    document.getElementById('error-cash_name').style.display = 'none';
+            }
+                        return isValid;
+    }    
+}
